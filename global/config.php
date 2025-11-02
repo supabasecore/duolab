e0e198a6875574ee7d83f6f1e2fa8300f57637b0
supabasecore <145882347+supabasecore@users.noreply.github.com>
2025-11-02 15:10:06 -0500

    add config dba

--- global/config.php
+++ global/config.php
@@ -1,9 +1,8 @@
 <?php 
-
-define("SCRIPT_VER",'260223_1');
-define("SERVER","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-define("DB","db_duolab");
-
+define("SCRIPT_VER", '260223_1');
+define("SERVER", "sql8.freesqldatabase.com");
+define("DB_USER", "sql8805770");
+define("DB_PASSWORD", "blS5fQz1Ve");
+define("DB", "sql8805770");
+define("DB_PORT", 3306);
 ?>
